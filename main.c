31f43b100316effee45e1af1c4f1a63e5765497c
RogerTWS02 <elia.falzoni@mail.polimi.it>
2023-06-22 01:30:40 +0200

    Aggiunte funzioni input handling

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+char buffer[5096];
+
+//Legge riga per riga da input redirection e le passa, una per una, a input_handler
+void input_reader(){
+    while(fgets(buffer, 5096, stdin))
+        input_handler(*buffer);
+    return;        
+}
+
+//Gestisce i comandi in input chiamando le funzioni giuste in base al comando iniettato
+void input_handler(char *input){
+    if(input[0]=='d')
+        demolisci_stazione(*input);
+    if(input[0]=='r')
+        rottama_auto(*input);
+    if(input[0]=='p')
+        pianifica_percorso(*input);
+    if(input[0]=='a'&&input[9]=='s')
+        aggiungi_stazione(*input);
+    if(input[0]=='a'&&input[9]=='a')
+        aggiungi_auto(*input);
+    return;
+}
+
 int main() {
-    
+    input_reader();
+    return 0;
 }
